c4a9f45b1426405784fb865e559d7d15b31dbba0
Sisi Li <vivianss0301@gmail.com>
2017-01-28 19:03:12 -0500

    updated server.js

--- server.js
+++ server.js
@@ -24,17 +24,11 @@
 
 // import the 3rd party connect module and
 // assign it as a reference to the connect variable
-<<<<<<< HEAD
+
 let express = require('express');
 
 // create an instance of the connect server - app
 let app = express();
-=======
-let connect = require('connect');
-
-// create an instance of the connect server - app
-let app = connect();
->>>>>>> b63fad330624c108f38900a4f73906c9c63cd08f
 
 // create the port constant
 const port = 3000;
@@ -47,29 +41,20 @@ console.log(`Server started at http://localhost:${port}`);
 
 // second route is '/hello'
 app.use('/hello', (req, res, next) => {
-<<<<<<< HEAD
+
   res.send("Hello, World!");
 
-=======
-  res.setHeader('Content-Type', 'text/plain');
-  res.end("Hello, World!");
-
   next();
->>>>>>> b63fad330624c108f38900a4f73906c9c63cd08f
+
 });
 
 // first route is '/' - root of my website
 app.use('/', (req, res, next) => {
-<<<<<<< HEAD
+
   res.send("Welcome!");
 
 });
 
 module.exports = app;
-=======
-  res.setHeader('Content-Type', 'text/plain');
-  res.end("Welcome!");
 
-  next();
-});
->>>>>>> b63fad330624c108f38900a4f73906c9c63cd08f
+
